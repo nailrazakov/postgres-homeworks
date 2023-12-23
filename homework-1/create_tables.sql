4efcd36b6a5fb34080d2dcd6d5e0d72bb8dd0fac
Nail Razakov Win10 <nailrazakov1982@gmail.com>
2023-12-23 03:38:24 +0300

    homework-1 create and fill tables

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,1 +1,24 @@
 -- SQL-команды для создания таблиц
+CREATE TABLE employees
+(
+	employee_id int PRIMARY KEY,
+	first_name varchar(20) NOT NULL,
+	last_name varchar(50) NOT NULL,
+	title varchar(100) NOT NULL,
+	birth_date date NOT NULL,
+	notes text NOT NULL
+);
+CREATE TABLE customers
+(
+	customer_id varchar(20) PRIMARY KEY,
+	company_name varchar(100) NOT NULL,
+	contact_name varchar(50) NOT NULL
+);
+CREATE TABLE orders
+(
+	order_id int PRIMARY KEY,
+	customer_id varchar(20) REFERENCES customers(customer_id) NOT NULL,
+	employee_id int REFERENCES employees(employee_id) NOT NULL,
+	order_date date NOT NULL,
+	ship_city varchar(50) NOT NULL
+);
