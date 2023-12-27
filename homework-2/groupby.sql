0889132b6bb9731e636ccd1e50f7854104a7d389
Nail Razakov Win10 <nailrazakov1982@gmail.com>
2023-12-27 13:23:59 +0300

    correction

--- homework-2/groupby.sql
+++ homework-2/groupby.sql
@@ -21,6 +21,7 @@ ORDER BY COUNT(*) DESC;
 -- 5. суммарный вес заказов (в которых известен регион) по странам, но вывести только те результаты, где суммарный вес на страну больше 2750. Отсортировать по убыванию суммарного веса (см таблицу orders, колонки ship_region, ship_country, freight)
 SELECT DISTINCT ship_country, SUM(freight)
 FROM orders
+WHERE ship_region IS NOT NULL
 GROUP BY ship_country
 HAVING SUM(freight) > 2750
 ORDER BY SUM(freight) DESC;
